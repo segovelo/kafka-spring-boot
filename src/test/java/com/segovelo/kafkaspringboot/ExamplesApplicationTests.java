2273f5930e84a5d81fccf15a6a1a950a8576a949
segovelo <gauchosinpingo@gmail.com>
2024-03-23 17:05:32 +0000

    Spring boot Application added.

---
+++ src/test/java/com/segovelo/kafkaspringboot/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segovelo.kafkaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
